9492406bd90c2fa77fe82f4b559e325a7d1619b5
Lennert Dael <lennert.dael@live.com>
2012-10-02 21:54:48 +0200

    first commit

---
+++ splitsen/splitsen/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  splitsen
+//
+//  Created by Davy Leemans on 02/10/12.
+//  Copyright (c) 2012 Davy Leemans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ splitsen/splitsen/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  splitsen
+//
+//  Created by Davy Leemans on 02/10/12.
+//  Copyright (c) 2012 Davy Leemans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
